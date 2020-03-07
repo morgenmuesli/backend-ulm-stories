1627f24417baf34c250b75519feac78e7ac08df3
Daria <daria.waldow@gmx.de>
2020-03-07 13:52:17 +0100

    change locations :D

--- ulm-stories/store/npcLocation.js
+++ ulm-stories/store/npcLocation.js
@@ -3,43 +3,43 @@ export const state = () => ({
     {
       characterID: "einstein",
       characterName: "Alber Einstein",
-      latlng: [48.39997, 9.99451],
+      latlng: [48.396018, 9.991371],
       chapter: 7
     },
     {
       characterID: "ensinger",
       characterName: "Matthäus Ensinger",
-      latlng: [48.398523, 9.992555],
+      latlng: [48.398673, 9.990946],
       chapter: 1
     },
     {
       characterID: "Aicher",
       characterName: "Otl Aicher ",
-      latlng: [48.39791801073818, 9.991607437432059],
+      latlng: [48.397866, 9.991598],
       chapter: 2
     },
     {
       characterID: "Streicher",
       characterName: "Agathe Streicher",
-      latlng: [48.395318, 9.990801],
+      latlng: [48.397497, 9.993279],
       chapter: 3
     },
     {
       characterID: "Berblinger",
       characterName: "Albrecht Ludwig Berblinger",
-      latlng: [48.395624, 9.993633],
+      latlng: [48.397045, 9.99348],
       chapter: 4
     },
     {
       characterID: "Holl",
       characterName: "Maria Holl ",
-      latlng: [48.396976, 9.991556],
+      latlng: [48.396973, 9.991561],
       chapter: 5
     },
     {
-      characterID: "Schwanenwirtin",
-      characterName: "Schwanenwirtin",
-      latlng: [48.397154, 9.98873],
+      characterID: "Schwanenwirten",
+      characterName: "Schwanenwirten",
+      latlng: [48.396282, 9.990509],
       chapter: 6
     }
   ]
