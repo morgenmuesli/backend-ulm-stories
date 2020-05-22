9970ce2bab39b4db11160d830aa290fc0e218724
Anna <a.parlapani@gmx.net>
2020-05-22 10:23:03 +0200

    changed to scss

--- ulm-stories/pages/game/phoneCall.vue
+++ ulm-stories/pages/game/phoneCall.vue
@@ -13,8 +13,8 @@
       <div class="container">
         <div
           :key="index"
-          class="columns is-mobile "
           v-for="(item, index) in messages"
+          class="columns is-mobile "
         >
           <div class="column is-four-fifths">
             <div class="card">
@@ -31,10 +31,10 @@
         <div class="columns is-center is-mobile">
           <div class="column is-three-quarters-mobile">
             <input
+              v-model="input"
               class="input"
               placeholder="Text input"
               type="text"
-              v-model="input"
             />
           </div>
           <div class="column is-mobile">
@@ -64,42 +64,45 @@ export default {
 };
 </script>
 
-<style lang="sass" scoped>
+<style lang="scss" scoped>
+h3 {
+  color: white;
+}
 
+.main {
+  height: 100vh;
+  max-height: 100vh;
+  max-width: 100%;
+}
 
-h3
-  color: white
+.chatRoom {
+  overflow-y: scroll;
+  max-width: 100%;
+}
 
-.main
-  height: 100vh
-  max-height: 100vh
-  max-width: 100%
+.card {
+  box-sizing: border-box;
+  border-radius: 0.1em 1.5em 0.5em 3em;
+  padding-left: 3em;
+}
 
-.chatRoom
-  overflow-y: scroll
-  max-width: 100%
+.container {
+  margin: 1em;
+}
 
-.card
-  box-sizing: border-box
-  border-radius: 0.1em 1.5em 0.5em 3em
-  padding-left: 3em
+.header {
+  position: static;
+  top: 0;
+  background-color: darkslateblue;
+  width: 100%;
+  display: flex;
+}
 
-.container
-  margin: 1em
-
-.header
-  position: static
-  top: 0
-  background-color: darkslateblue
-  width: 100%
-  display: flex
-
-
-.writingWindow
-
-  width: 100%
-  height: 10vh
-  background-color: gray
-  position: fixed
-  bottom: 0
+.writingWindow {
+  width: 100%;
+  height: 10vh;
+  background-color: gray;
+  position: fixed;
+  bottom: 0;
+}
 </style>
