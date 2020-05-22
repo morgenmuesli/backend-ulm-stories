72eea41b2b1fa26578df8509d9894a24aebeaac2
Christoph Meyer <christoph-1.meyer@uni-ulm.de>
2020-05-22 10:47:02 +0200

    remove anoying alert

---
+++ ulm-stories/store/videosprof.js
@@ -0,0 +1,31 @@
+export const state = () => ({
+  currentMessages: [],
+  profvideos: [
+    [
+      {
+        text:
+          "Du hast den jungen Ensinger wieder in seine Zeit zurück schicken können, stimmt‘s??"
+      },
+      {
+        text: "Das Chaos im Labor wurde nämlich gerade viel ruhiger!"
+      },
+      {
+        text:
+          "Mach weiter so! Schau auf der Karte nach, da müsste der nächste Ort frei geschaltet worden sein. Viel Erfolg und vergiss die Kristallscherben nicht!"
+      }
+    ],
+    [
+      {
+        text:
+          "Du hast den jungen Ensinger wieder in seine Zeit zurück schicken können, stimmt‘s??"
+      },
+      {
+        text: "Das Chaos im Labor wurde nämlich gerade viel ruhiger!"
+      },
+      {
+        text:
+          "Mach weiter so! Schau auf der Karte nach, da müsste der nächste Ort frei geschaltet worden sein. Viel Erfolg und vergiss die Kristallscherben nicht!"
+      }
+    ]
+  ]
+});
