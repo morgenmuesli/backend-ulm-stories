3228569aa2196905857996d11e9d6c1ad252d7ac
Christoph Meyer <christoph-1.meyer@uni-ulm.de>
2020-06-16 10:32:55 +0200

    Updates

--- sw.js
+++ sw.js
@@ -28,5 +28,5 @@ workbox.precaching.cleanupOutdatedCaches()
 // --------------------------------------------------
 
 // Register route handlers for runtimeCaching
-workbox.routing.registerRoute(new RegExp('/nuxtfiles/'), new workbox.strategies.CacheFirst ({}), 'GET')
-workbox.routing.registerRoute(new RegExp('/'), new workbox.strategies.NetworkFirst ({}), 'GET')
+workbox.routing.registerRoute(new RegExp('/backend-ulm-stories/nuxtfiles/'), new workbox.strategies.CacheFirst ({}), 'GET')
+workbox.routing.registerRoute(new RegExp('/backend-ulm-stories/'), new workbox.strategies.NetworkFirst ({}), 'GET')
